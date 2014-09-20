4a3d92800076654ebc9738788692e8bd153966bb
Eirik S. Morland <eirik@morland.no>
2014-09-20 13:34:35 +0200

    Add files needed for testling.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+require('should');
+var f = require('..');
+
+describe('Calculating alcohol', function() {
+  it('Should calculate the expected value', function() {
+    f(1.050, 1.010).should.equal(5.240000000000005);
+  });
+  it('Should throw an error if we do not supply OG', function() {
+    (function() {f()}).should.throw('No OG provided');
+  });
+});
